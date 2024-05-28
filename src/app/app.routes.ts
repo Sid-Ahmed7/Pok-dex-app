a3803d9dff7cac8e6dff5840694a7534e036a64b
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-05-28 16:30:28 +0200

    Add routes for PokedexPageComponent and CreatePokemonPageComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PokedexPageComponent } from './pages/pokedex-page/pokedex-page.component';
+import { CreatePokemonPageComponent } from './pages/create-pokemon-page/create-pokemon-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:PokedexPageComponent},
+    {path: 'create', component:CreatePokemonPageComponent}
+];
