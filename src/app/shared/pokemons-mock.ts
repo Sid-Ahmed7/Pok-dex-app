7486da39781769b8c07acadff22dd54427885aea
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-05-28 15:33:52 +0200

    Add Pokemon mock data file

---
+++ src/app/shared/pokemons-mock.ts
@@ -0,0 +1,178 @@
+import { Pokemon } from "./pokemon";
+
+export const POKEMONS: Pokemon[] = [
+    {
+        name: "Pikachu",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+        description: "Pikachu est un Pokémon de type Électrique. Il est connu pour ses joues qui produisent de l'électricité.",
+        types: ["Électrique"]
+    },
+    {
+        name: "Bulbizarre",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+        description: "Bulbizarre est un Pokémon de type Plante/Poison. Il porte une plante sur son dos qui grandit avec lui.",
+        types: ["Plante", "Poison"]
+    },
+    {
+        name: "Herbizarre",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png",
+        description: "Herbizarre est un Pokémon de type Plante/Poison. Sa plante sur le dos a grandi en une grande fleur.",
+        types: ["Plante", "Poison"]
+    },
+    {
+        name: "Florizarre",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png",
+        description: "Florizarre est un Pokémon de type Plante/Poison. La fleur sur son dos libère un parfum apaisant.",
+        types: ["Plante", "Poison"]
+    },
+    {
+        name: "Salamèche",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+        description: "Salamèche est un Pokémon de type Feu. La flamme sur sa queue représente son état de santé.",
+        types: ["Feu"]
+    },
+    {
+        name: "Reptincel",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png",
+        description: "Reptincel est un Pokémon de type Feu. Il est plus grand et sa queue brûle encore plus brillamment.",
+        types: ["Feu"]
+    },
+    {
+        name: "Dracaufeu",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png",
+        description: "Dracaufeu est un Pokémon de type Feu/Vol. Il crache des flammes capables de faire fondre des rochers.",
+        types: ["Feu", "Vol"]
+    },
+    {
+        name: "Carapuce",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+        description: "Carapuce est un Pokémon de type Eau. Il utilise sa carapace pour se protéger et pour nager rapidement.",
+        types: ["Eau"]
+    },
+    {
+        name: "Carabaffe",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png",
+        description: "Carabaffe est un Pokémon de type Eau. Il a grandi et sa carapace est devenue plus solide.",
+        types: ["Eau"]
+    },
+    {
+        name: "Tortank",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png",
+        description: "Tortank est un Pokémon de type Eau. Ses canons à eau peuvent percer l'acier.",
+        types: ["Eau"]
+    },
+    {
+        name: "Rondoudou",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/039.png",
+        description: "Rondoudou est un Pokémon de type Normal/Fée. Il chante des berceuses pour endormir ses adversaires.",
+        types: ["Normal", "Fée"]
+    },
+    {
+        name: "Grodoudou",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/040.png",
+        description: "Grodoudou est un Pokémon de type Normal/Fée. Son corps est incroyablement élastique.",
+        types: ["Normal", "Fée"]
+    },
+    {
+        name: "Mewtwo",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/150.png",
+        description: "Mewtwo est un Pokémon légendaire de type Psy. Il a été créé par manipulation génétique.",
+        types: ["Psy"]
+    },
+    {
+        name: "Evoli",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png",
+        description: "Evoli est un Pokémon de type Normal. Il a la capacité de s'adapter à de nombreux environnements.",
+        types: ["Normal"]
+    },
+    {
+        name: "Aquali",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/134.png",
+        description: "Aquali est un Pokémon de type Eau. Il est entouré d'une fine pellicule d'eau qui le protège.",
+        types: ["Eau"]
+    },
+    {
+        name: "Voltali",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/135.png",
+        description: "Voltali est un Pokémon de type Électrique. Il peut générer des décharges électriques puissantes.",
+        types: ["Électrique"]
+    },
+    {
+        name: "Pyroli",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/136.png",
+        description: "Pyroli est un Pokémon de type Feu. Il peut chauffer son corps à des températures extrêmes.",
+        types: ["Feu"]
+    },
+    {
+        name: "Mentali",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/196.png",
+        description: "Mentali est un Pokémon de type Psy. Il peut manipuler les ondes cérébrales de ses adversaires.",
+        types: ["Psy"]
+    },
+    {
+        name: "Noctali",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/197.png",
+        description: "Noctali est un Pokémon de type Ténèbres. Il peut se fondre dans l'obscurité et attaquer ses ennemis.",
+        types: ["Ténèbres"]
+    },
+    {
+        name: "Phyllali",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/470.png",
+        description: "Phyllali est un Pokémon de type Plante. Il peut absorber la lumière du soleil pour se régénérer.",
+        types: ["Plante"]
+    },
+    {
+        name: "Givrali",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/471.png",
+        description: "Givrali est un Pokémon de type Glace. Il peut geler l'air ambiant pour créer des cristaux de glace.",
+        types: ["Glace"]
+        },
+        {
+        name: "Goupix",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/037.png",
+        description: "Goupix est un Pokémon de type Feu. Ses six queues deviennent plus majestueuses à mesure qu'il grandit.",
+        types: ["Feu"]
+        },
+        {
+        name: "Feunard",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/038.png",
+        description: "Feunard est un Pokémon de type Feu. Ses queues brûlent d'une flamme bleue lorsqu'il est en colère.",
+        types: ["Feu"]
+        },
+        {
+        name: "Psykokwak",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/054.png",
+        description: "Psykokwak est un Pokémon de type Eau. Il souffre souvent de maux de tête sévères qui amplifient ses pouvoirs psychiques.",
+        types: ["Eau"]
+        },
+        {
+        name: "Akwakwak",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/055.png",
+        description: "Akwakwak est un Pokémon de type Eau. Son bec est capable de percer des rochers.",
+        types: ["Eau"]
+        },
+        {
+        name: "Roucarnage",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/018.png",
+        description: "Roucarnage est un Pokémon de type Normal/Vol. Ses ailes puissantes peuvent créer des tornades.",
+        types: ["Normal", "Vol"]
+        },
+        {
+        name: "Alakazam",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/065.png",
+        description: "Alakazam est un Pokémon de type Psy. Il a un QI extrêmement élevé et peut se souvenir de tout.",
+        types: ["Psy"]
+        },
+        {
+        name: "Lucario",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/448.png",
+        description: "Lucario est un Pokémon de type Combat/Acier. Il peut sentir et manipuler les auras des autres créatures.",
+        types: ["Combat", "Acier"]
+        },
+        {
+        name: "Carchacrok",
+        imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/445.png",
+        description: "Garchomp est un Pokémon de type Dragon/Sol. Il est incroyablement rapide, même en volant.",
+        types: ["Dragon", "Sol"]
+        }
+        ];
