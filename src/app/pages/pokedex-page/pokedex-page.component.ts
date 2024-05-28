a842b9cf8b12579072859d46089d059bb7e69c77
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-05-28 16:23:49 +0200

    Add onReceivePokemon method and import PokemonList and PokemonDetail component

--- src/app/pages/pokedex-page/pokedex-page.component.ts
+++ src/app/pages/pokedex-page/pokedex-page.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { Pokemon } from '../../shared/pokemon';
 import { PokedexService } from '../../shared/pokedex.service';
+import { PokemonDetailComponent } from '../../components/pokemon-detail/pokemon-detail.component';
+import { PokemonListComponent } from '../../components/pokemon-list/pokemon-list.component';
 
 @Component({
   selector: 'app-pokedex-page',
   standalone: true,
-  imports: [],
+  imports: [PokemonListComponent, PokemonDetailComponent],
   templateUrl: './pokedex-page.component.html',
   styleUrl: './pokedex-page.component.css'
 })
 export class PokedexPageComponent implements OnInit {
+  
   pokemons: Pokemon[] = [];
+  pokemonDetail!: Pokemon;
 
   pokemonService = inject(PokedexService);
 
@@ -18,6 +22,10 @@ export class PokedexPageComponent implements OnInit {
     this.pokemonService.getPokemons().subscribe(data => {
       this.pokemons = data;
     })
+  }
+
+  onReceivePokemon(event: Pokemon) {
+    this.pokemonDetail = event
   }
 
 }
